81c393eb12b93e189dd227483baf355f90ec217f
SaiRakindi <sai.rakindi3@gmail.com>
2023-11-22 17:45:29 +0530

    added right bar in dashboard

--- app/ui/dashboard/rightbar/rightbar.jsx
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -1,5 +1,49 @@
+import Image from "next/image";
+import styles from "./rightbar.module.css";
+import { MdPlayCircleFilled, MdReadMore } from "react-icons/md";
+
 const Rightbar = () => {
-  return <div>rightbar</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.bgContainer}>
+          <Image src="/astronaut.png" alt="" fill className={styles.bgImage} />
+        </div>
+        <div className={styles.text}>
+          <span className={styles.notification}>🔥 Available Now</span>
+          <h3 className={styles.title}>
+            How to use the new version of the admin dashboard?
+          </h3>
+          <span className={styles.subtitle}>Takes 4 minutes to learn</span>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio neque
+            veritatis expedita hic voluptatem!
+          </p>
+          <button className={styles.button}>
+            <MdPlayCircleFilled /> Watch
+          </button>
+        </div>
+      </div>
+
+      <div className={styles.item}>
+        <div className={styles.bgContainer}></div>
+        <div className={styles.text}>
+          <span className={styles.notification}>🚀 Coming Soon</span>
+          <h3 className={styles.title}>
+            New server actions are available, partial pre-rending is coming up!{" "}
+          </h3>
+          <span className={styles.subtitle}>Boost your productivity</span>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+            temporibus amet tempore ipsum non maxime, autem labore.
+          </p>
+          <button className={styles.button}>
+            <MdReadMore /> Watch
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Rightbar;
